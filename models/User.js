1dc4d6fa20b88bee37847bde6a793f9539a00380
Yash Katkar <121810932+katkaryk@users.noreply.github.com>
2024-06-16 16:03:12 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isVerified: { type: Boolean, default: false },
+  isVerified: { type: Boolean, default: true },
   verificationToken: { type: String },
 });
 
