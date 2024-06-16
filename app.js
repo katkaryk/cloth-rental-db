69ecb140167cf28862ab332489e9c8960586acbc
Yash Katkar <121810932+katkaryk@users.noreply.github.com>
2024-06-16 16:40:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
 import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
 import cartRoutes from './routes/cartRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods: GET,POST,OPTIONS,DELETE,PUT");
+  res.header("Access-Control-Allow-Origin' 'http://localhost:5000' always");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 app.use(express.json());
-
+app.use(cors());
 app.use('/api/users', userRoutes);
 app.use('/api/cart', cartRoutes);
 
